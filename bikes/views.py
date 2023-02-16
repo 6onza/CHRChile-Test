857f51816d0334164fc19f9e2ccb86d8047cdddf
6onza <gvineg@fi.uba.ar>
2023-02-16 15:35:18 -0300

    Refactor: bikes data saving view

--- bikes/views.py
+++ bikes/views.py
@@ -10,39 +10,38 @@ import datetime
 class SaveBikesDataView(View):
     def get(self, request):
         data = get_bike_data()
-        if data:
-            if not Company.objects.filter(name=data['network']['company'][0]).exists():
-                company = Company.objects.create(
-                    name=data['network']['company'][0],
-                    gbfs_href=data['network']['gbfs_href'],
-                )
-            else:
-                company = Company.objects.get(name=data['network']['company'][0])
+        if not data:
+            return HttpResponse('Error!', status=500)
 
-            for station in data['network']['stations']:
-                Station.objects.create(
-                    company=company,
-                    name=station['name'],
-                    latitude=station['latitude'],
-                    longitude=station['longitude'],
-                    free_bikes=station['free_bikes'],
-                    empty_slots=station['empty_slots'],
-                    timestamp=station['timestamp'],
-                    address=station['extra']['address'],
-                    altitude=station['extra']['altitude'],
-                    ebikes=station['extra']['ebikes'],
-                    has_ebikes=station['extra']['has_ebikes'],
-                    last_updated=station['extra']['last_updated'],
-                    payment=', '.join(station['extra']['payment']),
-                    payment_terminal=station['extra']['payment-terminal'],
-                    renting=station['extra']['renting'],
-                    returning=station['extra']['returning'],
-                    slots=station['extra']['slots'],
-                )
-        else:
-            HttpResponse('Error!', status=500)
+        company_name = data['network']['company'][0]
+        company, created = Company.objects.get_or_create(
+            name=company_name,
+            defaults={'gbfs_href': data['network']['gbfs_href']}
+        )
+
+        for station in data['network']['stations']:
+            Station.objects.create(
+                company=company,
+                name=station['name'],
+                latitude=station['latitude'],
+                longitude=station['longitude'],
+                free_bikes=station['free_bikes'],
+                empty_slots=station['empty_slots'],
+                timestamp=station['timestamp'],
+                address=station['extra']['address'],
+                altitude=station['extra']['altitude'],
+                ebikes=station['extra']['ebikes'],
+                has_ebikes=station['extra']['has_ebikes'],
+                last_updated=station['extra']['last_updated'],
+                payment=', '.join(station['extra']['payment']),
+                payment_terminal=station['extra']['payment-terminal'],
+                renting=station['extra']['renting'],
+                returning=station['extra']['returning'],
+                slots=station['extra']['slots'],
+            )
 
         return HttpResponse('Data saved!', status=200)
+
 
 
 class BikesDataView(View):
