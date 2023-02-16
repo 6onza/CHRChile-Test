d1c1df7ebf3c3e40a7123bd401051ce2bb448620
6onza <gvineg@fi.uba.ar>
2023-02-16 10:00:12 -0300

    Fix: now get_bike_data() handle http errors

--- bikes/utils.py
+++ bikes/utils.py
@@ -4,8 +4,16 @@ API_BIKES_URL = 'http://api.citybik.es/v2/networks/bikesantiago'
 
 def get_bike_data():
     url = API_BIKES_URL
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+        
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    
+    except requests.exceptions.RequestException as err:
+        print(f'Other error occurred: {err}')
+    
     if response.status_code == 200:
-        return response.json()
+            return response.json()
     else:
         return None
