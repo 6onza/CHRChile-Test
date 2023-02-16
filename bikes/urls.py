10da6cdffe0ba873f3cb2a9d42dd8725d6db8e4b
6onza <gvineg@fi.uba.ar>
2023-02-16 08:38:38 -0300

    Fix: changed url to store bikes data

--- bikes/urls.py
+++ bikes/urls.py
@@ -3,6 +3,6 @@ from .views import SaveBikesDataView, BikesDataView
 
 
 urlpatterns = [
-    path('save-bikes-data/', SaveBikesDataView.as_view(), name='save_bikes_data'),
-    path('bikes-data/', BikesDataView.as_view(), name='bikes_data'),
+    path('store-bikes-data/', SaveBikesDataView.as_view(), name='store_bikes_data'),
+    path('bikes-stations/', BikesDataView.as_view(), name='bikes_stations'),
 ]
